e7568f18b7797aa986807c67115ab73e217b042d
Moises <moiseslopez1155@gmail.com>
2026-03-04 19:44:26 -0600

    Ingreso datos.

--- Program.cs
+++ Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿Dictionary<string, string> usuarios = new Dictionary<string, string>();
+
+string usuario;
+
+Console.WriteLine("Ingrese la cantidad de usarios que ingresara: ");
+int cant = int.Parse(Console.ReadLine());
+
+for (int x = 0; x < cant; x++)
+{
+    Console.WriteLine("Ingrese el usuario: ");
+    usuario = Console.ReadLine();
+    Console.WriteLine("Ingrese la contraseña: ");
+    string contraseña = Console.ReadLine();
+
+    usuarios.Add(usuario, contraseña);
+}
